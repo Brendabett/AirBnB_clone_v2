9e0bdb87930d407ce84c8734930dfb6e86cfa7ab
Brendabett <98813715+Brendabett@users.noreply.github.com>
2022-09-28 21:10:29 +0300

    My commit

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,8 +3,8 @@
 from fabric.api import *
 import os.path
 
-env.user = "ubuntu"
-env.hosts = ["104.196.155.240", "34.74.146.120"]
+env.user = 'ubuntu'
+env.hosts = ["34.229.64.49", "3.234.242.109"]
 env.key_filename = "~/id_rsa"
 
 
@@ -18,13 +18,12 @@ def do_deploy(archive_path):
         base = arc[1].strip('.tgz')
         put(archive_path, '/tmp/')
         sudo('mkdir -p /data/web_static/releases/{}'.format(base))
-        main = '/data/web_static/releases/{}'.format(base)
+        main = "/data/web_static/releases/{}".format(base)
         sudo('tar -xzf /tmp/{} -C {}/'.format(arc[1], main))
         sudo('rm /tmp/{}'.format(arc[1]))
         sudo('mv {}/web_static/* {}/'.format(main, main))
         sudo('rm -rf /data/web_static/current')
-        sudo('ln -s {}/ /data/web_static/current'.format(main))
+        sudo('ln -s {}/ "/data/web_static/current"'.format(main))
         return True
     except:
         return False
-
