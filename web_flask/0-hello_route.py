96ee837f1507b6ac154c2638df4b26198d7aaba8
Brendabett <98813715+Brendabett@users.noreply.github.com>
2022-11-08 13:33:46 +0300

    my commit

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Starts a Flask web application.
-The application listens on 0.0.0.0, port 5000.
+The application listens on 127.0.0.1, port 5000.
 Routes:
     /: Displays 'Hello HBNB!'
 """
